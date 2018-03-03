a3ef64fe30745edc1c37d9e027bdc0d5378aaa6f
xromp <penaflor024@gmail.com>
2018-03-03 21:11:03 +0800

    retrack origin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MaterialDesignModule } from './modules/material-design/material-design.module';
 
 import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
@@ -25,14 +23,12 @@ import { MasterModule } from './modules/master/master.module';
     FormsModule,
     HttpModule,
     RouterModule,
-    MaterialDesignModule,
     AppRoutingModule,
     DashboardModule,
     ProfileModule,
     MasterModule
   ],
   providers: [],
-  schemas: [NO_ERRORS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
